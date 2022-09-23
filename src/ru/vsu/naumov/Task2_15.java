8cd6010bdd7dabd4da604df543735b3af73c660c
capti <captidelta@gmail.com>
2022-09-23 23:14:51 +0300

    Add files via upload

--- src/ru/vsu/naumov/Task2_15.java
+++ src/ru/vsu/naumov/Task2_15.java
@@ -23,7 +23,7 @@ public class Task2_15 {
 
 
 }
-
+    // сам треугольник
 class Triangle {
     private final double sideA;
     private final double sideB;
@@ -35,16 +35,22 @@ class Triangle {
         this.sideB = sideB;
         this.sideC = sideC;
 
+        // пишет что треугольник существовать не может при таких значениях
+
         if (checkPossibilityExist()) {
             throw new IllegalArgumentException("Треугольник не может существовать");
         }
 
     }
 
+
+    // считает полупериметр
+
     public double calculateHalfPerimeter() {
         return (sideA + sideB + sideC) / 2;
     }
 
+    // проверяет существование треугольника
     private boolean checkPossibilityExist() {
         return (sideA >= sideB + sideC || sideB >= sideA + sideC || sideC >= sideA + sideB);
 
