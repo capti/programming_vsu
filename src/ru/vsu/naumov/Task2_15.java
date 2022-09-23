339c894367815a690d6b04115c209eb74e115712
capti <captidelta@gmail.com>
2022-09-23 23:11:43 +0300

    Add files via upload

--- src/ru/vsu/naumov/Task2_15.java
+++ src/ru/vsu/naumov/Task2_15.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task2_15 {
 
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Длина стороны A  = ");
@@ -18,29 +18,49 @@ public class Task2_15 {
         System.out.print("Радиус R  = ");
         double radius = scanner.nextDouble();
 
+
     }
 
 
-    private record Triangle(double sideA, double sideB, double sideC) {
-            private Triangle(double sideA, double sideB, double sideC) {
+}
 
-                this.sideA = sideA;
-                this.sideB = sideB;
-                this.sideC = sideC;
+class Triangle {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
 
-                if (checkPossibilityExist()) {
-                    throw new IllegalArgumentException("Треугольник не может существовать");
-                }
+    public Triangle(double sideA, double sideB, double sideC) {
 
-            }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
 
-            boolean checkPossibilityExist() {
-                return (sideA >= sideB + sideC || sideB >= sideA + sideC || sideC >= sideA + sideB);
-
-            }
-
-
+        if (checkPossibilityExist()) {
+            throw new IllegalArgumentException("Треугольник не может существовать");
         }
+
+    }
+
+    public double calculateHalfPerimeter() {
+        return (sideA + sideB + sideC) / 2;
+    }
+
+    private boolean checkPossibilityExist() {
+        return (sideA >= sideB + sideC || sideB >= sideA + sideC || sideC >= sideA + sideB);
+
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
 
 
 }
